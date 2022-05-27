92bfcd3a6e726c1b685623716990412aff86fde5
Kasun Thennakoon <kasun.thennakoon@itelasoft.com>
2022-05-28 00:00:08 +0530

    optimizations

--- app/src/main/java/com/kc/starwarsapidemo/presentation/home/adapter/PlanetsAdapter.kt
+++ app/src/main/java/com/kc/starwarsapidemo/presentation/home/adapter/PlanetsAdapter.kt
@@ -4,24 +4,29 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.kc.starwarsapidemo.R
 import com.kc.starwarsapidemo.data.model.Planet
+import com.kc.starwarsapidemo.databinding.ItemPlanetBinding
 
 class PlanetsAdapter(var context: Context) : RecyclerView.Adapter<PlanetViewHolder>() {
     private var planets: MutableList<Planet>? = mutableListOf()
     private var selectedIndex = -1
     private var onTopicClick: ((Planet, Int) -> Unit)? = null
+    private lateinit var binding: ItemPlanetBinding
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlanetViewHolder =
-        PlanetViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_planet, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlanetViewHolder {
+        binding = ItemPlanetBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PlanetViewHolder(
+            binding.root
         )
+    }
+
 
     override fun getItemCount(): Int = planets?.size ?: 0
 
     override fun onBindViewHolder(holder: PlanetViewHolder, position: Int) {
+        holder.setIsRecyclable(false)
         planets?.get(position)?.let { book ->
-            holder.bind(book, selectedIndex == position, context) {
+            holder.bind(book, selectedIndex == position, binding, context) {
                 onTopicClick?.invoke(book, position)
             }
         }
